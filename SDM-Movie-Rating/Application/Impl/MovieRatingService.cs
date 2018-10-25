2e8700cc4d532a88a674c309e06045d3150f4f84
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-10-25 11:42:27 +0200

    implemented interface

--- SDM-Movie-Rating/Application/Impl/MovieRatingService.cs
+++ SDM-Movie-Rating/Application/Impl/MovieRatingService.cs
@@ -1,10 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace SDM_Movie_Rating.Application.Impl
 {
     public class MovieRatingService: IMovieRatingService
     {
-        public MovieRatingService()
+
+        public double AverageGradeOfMovie(int movieId)
         {
+            throw new NotImplementedException();
+        }
+
+        public double AverageGradeOfReviewer(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CountMoviesWithGradeByReviewer(int reviewerId, int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CountReviewersOfMovie(int movieId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CountReviewsOfReviewer(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CountWhereMovieHasGrade(int movieId, int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetMoviesReviewedByReviewer(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetMoviesWithAverageHighestGrade(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetMoviesWithMostGradesOfFive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetReviewersWithMostReviewsDone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetReviewerWhoReviewedMovie(int movieId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
