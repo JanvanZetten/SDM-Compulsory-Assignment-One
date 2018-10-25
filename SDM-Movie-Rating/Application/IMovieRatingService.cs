460c9f66dd302ce9f2e53acc126bef6f6e15f642
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-10-25 11:37:38 +0200

    Added methods from Assignment to interface

--- SDM-Movie-Rating/Application/IMovieRatingService.cs
+++ SDM-Movie-Rating/Application/IMovieRatingService.cs
@@ -1,7 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace SDM_Movie_Rating.Application
 {
     public interface IMovieRatingService
     {
+        int CountReviewsOfReviewer(int reviewerId);
+        double AverageGradeOfReviewer(int reviewerId);
+        int CountMoviesWithGradeByReviewer(int reviewerId, int grade);
+        int CountReviewersOfMovie(int movieId);
+        double AverageGradeOfMovie(int movieId);
+        int CountWhereMovieHasGrade(int movieId, int grade);
+        List<int> GetMoviesWithMostGradesOfFive();
+        List<int> GetReviewersWithMostReviewsDone();
+        List<int> GetMoviesWithAverageHighestGrade(int amount);
+        List<int> GetMoviesReviewedByReviewer(int reviewerId);
+        List<int> GetReviewerWhoReviewedMovie(int movieId);
     }
 }
