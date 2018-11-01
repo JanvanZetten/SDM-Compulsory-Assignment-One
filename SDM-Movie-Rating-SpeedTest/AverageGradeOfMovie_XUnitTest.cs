8cfbe4b2fa18e60c2e4e9c1003ace9d714226f8f
Husso1997 <hussain1961@hotmail.com>
2018-11-01 10:43:22 +0100

    AverageGradeOfMovie Speeed Test

---
+++ SDM-Movie-Rating-SpeedTest/AverageGradeOfMovie_XUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using SDM_Movie_Rating.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SDM_Movie_Rating_SpeedTest
+{   [Collection("SpeedTest")]
+    public class AverageGradeOfMovie_XUnitTest :IClassFixture<DisposableMovieRating>
+    {
+        private IMovieRatingService _movieRatingService;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public AverageGradeOfMovie_XUnitTest(DisposableMovieRating disposableMovieRating,
+            ITestOutputHelper outputHelper)
+        {
+            _movieRatingService = disposableMovieRating.GetMovieRatingService();
+            _outputHelper = outputHelper;
+        }
+        [Theory]
+        [InlineData(2066490)]
+        public void SpeedTest(int movieId)
+        {
+            Assert.True(Timer.GetUserCPUTime(() =>
+            {
+                _movieRatingService.AverageGradeOfMovie(movieId);
+            }, _outputHelper) < 4000);
+        }
+    }
+}
