81a28647dc16754d2749f7f361ee73d99f3ecaf9
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-11-01 11:19:25 +0100

    SpeedTest 8 & 11

---
+++ SDM-Movie-Rating-SpeedTest/11-GetReviewerWhoReviewedMovie_XunitTest.cs
@@ -0,0 +1,35 @@
+﻿using SDM_Movie_Rating.Application;
+using SDM_Movie_Rating.Application.Impl;
+using SDM_Movie_Rating.Domain;
+using SDM_Movie_Rating_JsonReader;
+using System;
+using System.Diagnostics;
+using Xunit.Abstractions;
+using Xunit;
+
+// Disable Parrallelization for more accurate time test.
+namespace SDM_Movie_Rating_SpeedTest
+{
+    [Collection("SpeedTest")]
+    public class GetReviewerWhoReviewedMovie_XunitTest
+    {
+        private IMovieRatingService _movieRatingService;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public GetReviewerWhoReviewedMovie_XunitTest(ITestOutputHelper outputHelper, DisposableMovieRating disposeableMovieRating)
+        {
+            _outputHelper = outputHelper;
+            _movieRatingService = disposeableMovieRating.GetMovieRatingService();
+        }
+
+        [Theory]
+        [InlineData(2066490)]
+        [InlineData(2519299)]
+        [InlineData(2425137)]
+        [InlineData(305344)]
+        public void Test(int reviewerId)
+        {
+            Assert.True(Timer.GetUserCPUTime(() => { _movieRatingService.GetReviewerWhoReviewedMovie(reviewerId); }, _outputHelper) < 4000);
+        }
+    }
+}
