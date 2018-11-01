a630742cfd7b2c180fc15a0ed11f85780844a7c0
Husso1997 <hussain1961@hotmail.com>
2018-11-01 10:11:21 +0100

    Speed test

---
+++ SDM-Movie-Rating-SpeedTest/7-CountWhereMovieHasGrade.cs
@@ -0,0 +1,37 @@
+﻿using SDM_Movie_Rating.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SDM_Movie_Rating_SpeedTest
+{
+    [assembly: CollectionBehavior(DisableTestParallelization = true)]
+    public class CountWhereMovieHasGrade : IClassFixture<DisposableMovieRating>
+    {
+        private IMovieRatingService _movieRatingService;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public CountWhereMovieHasGrade(DisposableMovieRating DisposableMovieRating, 
+            ITestOutputHelper outputHelper)
+        {
+            _movieRatingService = DisposableMovieRating.GetMovieRatingService();
+            _outputHelper = outputHelper;
+        }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        public void SpeedTest(int movieId)
+        {
+            Assert.True(Timer.GetUserCPUTime(() =>
+            {
+                _movieRatingService.CountWhereMovieHasGrade(movieId, 1);
+
+            },_outputHelper) < 4000);
+        }
+
+    }
+}
