1eb2d88e31a2cbb8b952008ac055c32aaa7b8396
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-11-01 10:49:07 +0100

    SpeedTest 4

---
+++ SDM-Movie-Rating-SpeedTest/4-CountReviewersOfMovie_XunitTest.cs
@@ -0,0 +1,30 @@
+﻿using SDM_Movie_Rating.Application;
+using Xunit.Abstractions;
+using Xunit;
+
+// Disable Parrallelization for more accurate time test.
+namespace SDM_Movie_Rating_SpeedTest
+{
+    [Collection("SpeedTest")]
+    public class CountReviewersOfMovie_XunitTest : IClassFixture<DisposableMovieRating>
+    {
+        private IMovieRatingService _movieRatingService;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public CountReviewersOfMovie_XunitTest(ITestOutputHelper outputHelper, DisposableMovieRating disposeableMovieRating)
+        {
+            _outputHelper = outputHelper;
+            _movieRatingService = disposeableMovieRating.GetMovieRatingService();
+        }
+        
+        [Theory]
+        [InlineData(2066490)]
+        [InlineData(2519299)]
+        [InlineData(2425137)]
+        [InlineData(305344)]
+        public void Test(int movieId)
+        {
+            Assert.True(Timer.GetUserCPUTime(() => { _movieRatingService.CountReviewersOfMovie(movieId); }, _outputHelper) < 4000);
+        }
+    }
+}
--- SDM-Movie-Rating-SpeedTest/CountReviewsOfReviewer_XunitTest.cs
+++ SDM-Movie-Rating-SpeedTest/CountReviewsOfReviewer_XunitTest.cs
@@ -29,9 +29,9 @@ namespace SDM_Movie_Rating_SpeedTest
         [InlineData(100)]
         [InlineData(571)]
         [InlineData(1000)]
-        public void Test(int movieId)
+        public void Test(int reviewerId)
         {
-            Assert.True(Timer.GetUserCPUTime(() => { _movieRatingService.CountReviewsOfReviewer(movieId); }, _outputHelper) < 4000);
+            Assert.True(Timer.GetUserCPUTime(() => { _movieRatingService.CountReviewsOfReviewer(reviewerId); }, _outputHelper) < 4000);
         }
     }
 }
