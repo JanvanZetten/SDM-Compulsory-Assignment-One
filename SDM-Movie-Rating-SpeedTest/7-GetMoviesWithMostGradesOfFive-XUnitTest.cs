b28bd1f256fdd5446f48c3f13911824ebf8d44e7
Husso1997 <hussain1961@hotmail.com>
2018-11-01 11:12:06 +0100

    push

---
+++ SDM-Movie-Rating-SpeedTest/7-GetMoviesWithMostGradesOfFive-XUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using SDM_Movie_Rating.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SDM_Movie_Rating_SpeedTest
+{
+    [Collection("SpeedTest")]
+    public class _7_GetMoviesWithMostGradesOfFive_XUnitTest
+    {
+        private IMovieRatingService _movieRatingService;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public _7_GetMoviesWithMostGradesOfFive_XUnitTest(ITestOutputHelper outputHelper, 
+            DisposableMovieRating disposeableMovieRating)
+        {
+            _outputHelper = outputHelper;
+            _movieRatingService = disposeableMovieRating.GetMovieRatingService();
+        }
+
+        [Fact]
+        public void SpeedTest()
+        {
+            Assert.True(Timer.GetUserCPUTime(() =>
+            {
+                _movieRatingService.GetMoviesWithMostGradesOfFive();
+
+            }, _outputHelper) < 4000);
+        }
+    }
+}
